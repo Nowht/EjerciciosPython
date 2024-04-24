c3e902b73fced75c44a3f8d4bd26e9cdbc518860
Humberto <167370132+Nowht@users.noreply.github.com>
2024-04-24 16:12:21 -0600

    Add files via upload

--- Prueba corta 1.py
+++ Prueba corta 1.py
@@ -1,7 +1,61 @@
-#Corregir 1
+###CORREGUIR 1###
+# contraseña = input("Introduce la contraseña: ")
+# if contraseña == 'sesamo':
+#   print('Pasa')
+# else:
+#   print('No pasa')
 
-contraseña = input("Introduce la contraseña: ")
-if contraseña == "sesamo":
-    print("pasa")
-else:
-    print("No pasa")
+###CORREGUIR 2###
+# def aplica_iva(base):
+#     iva = 21
+#     base = base * iva   
+#     return base 
+
+# base = int(input('Introduce la base imponible de la factura: '))
+# print(aplica_iva(base))
+
+
+###CORREGUIR 3###
+
+# u = [1, 2, 3]
+# v = [4, 5, 6]
+
+# def producto_escalar(u,v):
+#     for i in range(len(u)):
+#         u[i] *= v[i] 
+#     return sum(u)
+
+# print(producto_escalar(u,v))
+
+
+###CORREGUIR 4###
+# listin = {'Juan':123456789, 'Pedro':987654321}
+
+# def elimina(listin, usuario):
+#     return listin.pop(usuario)
+
+# print(elimina(listin, 'Pedro'))
+
+
+
+###CORREGUIR 5###
+
+a = [(1, 2, 3),
+     (3, 2, 1)]
+b = [(1, 2),
+     (3, 4),
+     (5, 6)]
+
+def producto(a, b):
+    producto = []
+    fila = []
+    for i in range(len(b)):
+        for j in range(len(a)):
+            suma = 0
+            for k in range(len(a)):
+                suma += a[j][k] * b[j][k]
+            fila.append(suma)
+        producto.append(tuple(fila))
+    return tuple(producto)
+
+print(producto(a,b))
